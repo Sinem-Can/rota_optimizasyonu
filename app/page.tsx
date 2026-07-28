7be6d41fb017a9a054f0697aa8a1f34220284afe
ZelalDev <zelall2222@gmail.com>
2026-07-28 14:05:21 +0300

    onboarding eklendi

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { RouteDashboard } from '@/components/route-dashboard'
+import { OnboardingModal } from '@/components/onboarding-modal'
 
 export default function Page() {
-  return <RouteDashboard />
+  return (
+    <>
+      <RouteDashboard />
+      <OnboardingModal />
+    </>
+  )
 }
